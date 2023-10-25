2c96a5387259cd60b9db5229c2fb81861dcd1105
psh3055 <psh3055@gmail.com>
2023-10-26 01:54:49 +0900

    [WIP]

--- Project Daydream/Assets/Scripts/Player/SkillController.cs
+++ Project Daydream/Assets/Scripts/Player/SkillController.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class SkillController : MonoBehaviour
 {
+    SpriteRenderer sr;
+    public GameObject go;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sr = go.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("f"))
+            sr.material.color = Color.blue;
+
     }
 }
