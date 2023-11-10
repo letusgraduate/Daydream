d11dc7c817d1793885863d6c9d75e724e363edc7
Arkru <35932023+Arkru1204@users.noreply.github.com>
2023-11-10 21:58:25 +0900

    [Refactor] MapManager 리팩토링
    * 변수명 일괄 변경
    left -> first
    center -> secend
    right -> third
    * 일부 기능들 함수로 변경
    * 주석 추가
    * 오브젝트 이름 서치를 태그 서치로 변경
    * 피벗 오브젝트명 Pivot In/Out으로 변경
    * 피벗 오브젝트 색 변경

--- Project Daydream/Assets/Scripts/Map/MapManager.cs
+++ Project Daydream/Assets/Scripts/Map/MapManager.cs
@@ -5,97 +5,90 @@ using UnityEngine;
 public class MapManager : MonoBehaviour
 {
     /* --------------- 핀 포인트 --------------- */
-    private Transform leftPivot;
-    private Transform centerLeftPivot;
-    private Transform centerRightPivot;
-    private Transform rightPivot;
+    private Transform firstInPivot;
+    private Transform secendOutPivot;
+    private Transform secendInPivot;
+    private Transform thirdOutPivot;
 
-    /* ------ 활성화된 오브젝트의 자식들 ------- */
-    private Transform[] leftChildren;
-    private Transform[] centerChildren;
-    private Transform[] rightChildren;
-
-    /* -------------- 현재 랜덤 맵 ------------- */
-    private int nowLeftMap;
-    private int nowCenterMap;
-    private int nowRightMap;
+    /* ---------- 현재 랜덤 맵 인덱스 ---------- */
+    private int nowFirstMap;
+    private int nowSecendMap;
+    private int nowThirdMap;
 
     /* ---------------- 인스펙터 --------------- */
     [Header("오브젝트 연결")]
     [SerializeField]
-    private List<Transform> leftMaps = new List<Transform>();
+    private List<Transform> firstMaps = new List<Transform>();
     [SerializeField]
-    private List<Transform> centerMaps = new List<Transform>();
+    private List<Transform> secendMaps = new List<Transform>();
     [SerializeField]
-    private List<Transform> rightMaps = new List<Transform>();
+    private List<Transform> thirdMaps = new List<Transform>();
 
+    /* -------------- 이벤트 함수 -------------- */
     private void Start()
     {
         RandomMapSpawn();
     }
 
+    /* --------------- 기능 함수 --------------- */
     public void RandomMapSpawn()
     {
-        // 맵 없으면 탈출
-        if (leftMaps.Count <= 0 && centerMaps.Count <= 0 && rightMaps.Count <= 0)
+        /* 맵 없으면 탈출 */
+        if (firstMaps.Count <= 0 && secendMaps.Count <= 0 && thirdMaps.Count <= 0)
         {
             Debug.Log("Map Null");
             return;
         }
 
-        nowLeftMap = MapSelect(leftMaps);
-        nowCenterMap = MapSelect(centerMaps);
-        nowRightMap = MapSelect(rightMaps);
+        /* 랜덤 맵 선택 */
+        nowFirstMap = SelectMap(firstMaps);
+        nowSecendMap = SelectMap(secendMaps);
+        nowThirdMap = SelectMap(thirdMaps);
 
-        leftChildren = leftMaps[nowLeftMap].gameObject.GetComponentsInChildren<Transform>();
-        centerChildren = centerMaps[nowCenterMap].gameObject.GetComponentsInChildren<Transform>();
-        rightChildren = rightMaps[nowRightMap].gameObject.GetComponentsInChildren<Transform>();
+        /* 피벗 참조 */
+        firstInPivot = getPivot(firstMaps[nowFirstMap], "MapPivotIn");
+        secendOutPivot = getPivot(secendMaps[nowSecendMap], "MapPivotOut");
+        secendInPivot = getPivot(secendMaps[nowSecendMap], "MapPivotIn");
+        thirdOutPivot = getPivot(thirdMaps[nowThirdMap], "MapPivotOut");
 
-        /* 활성화된 오브젝트의 자식들 중 핀 포인트 찾기 */
-        for (int i = 0; i < leftChildren.Length; i++)
-        {
-            if (leftChildren[i].name == "LeftToCenterPivot")
-                leftPivot = leftChildren[i];
-        }
-        for (int i = 0; i < centerChildren.Length; i++)
-        {
-            if (centerChildren[i].name == "LeftToCenterPivot")
-                centerLeftPivot = centerChildren[i];
-        }
-        for (int i = 0; i < centerChildren.Length; i++)
-        {
-            if (centerChildren[i].name == "CenterToRightPivot")
-                centerRightPivot = centerChildren[i];
-        }
-        for (int i = 0; i < rightChildren.Length; i++)
-        {
-            if (rightChildren[i].name == "CenterToRightPivot")
-                rightPivot = rightChildren[i];
-        }
+        /* 1섹터 2섹터 연결 */
+        Vector3 secendPivotPos = secendMaps[nowSecendMap].position;
+        secendPivotPos.x += firstInPivot.position.x - secendOutPivot.position.x
+            + secendOutPivot.localScale.x / 2 + firstInPivot.localScale.x / 2; // 피벗 크기들의 절반씩 이동해 옆에 붙게
+        secendPivotPos.y += firstInPivot.position.y - secendOutPivot.position.y;
+        secendMaps[nowSecendMap].position = secendPivotPos;
 
-        // 왼쪽과 중앙 연결
-        Vector3 centerObjPosition = centerMaps[nowCenterMap].gameObject.transform.position;
-        centerObjPosition.x += leftPivot.position.x - centerLeftPivot.position.x + centerLeftPivot.localScale.x / 2 + leftPivot.localScale.x / 2;
-        centerObjPosition.y += leftPivot.position.y - centerLeftPivot.position.y;
-        centerMaps[nowCenterMap].gameObject.transform.position = centerObjPosition;
+        /* 2섹터 3섹터 연결 */
+        Vector3 thirdPivotPos = thirdMaps[nowThirdMap].position;
+        thirdPivotPos.x += secendInPivot.position.x - thirdOutPivot.position.x
+            + secendInPivot.localScale.x / 2 + thirdOutPivot.localScale.x / 2;
+        thirdPivotPos.y += secendInPivot.position.y - thirdOutPivot.position.y;
+        thirdMaps[nowThirdMap].position = thirdPivotPos;
 
-        // 중앙과 오른쪽 연결
-        Vector3 rightObjPosition = rightMaps[nowRightMap].gameObject.transform.position;
-        rightObjPosition.x += centerRightPivot.position.x - rightPivot.position.x + centerRightPivot.localScale.x / 2 + rightPivot.localScale.x / 2;
-        rightObjPosition.y += centerRightPivot.position.y - rightPivot.position.y;
-        rightMaps[nowRightMap].gameObject.transform.position = rightObjPosition;
-
-        RemoveMapList(leftMaps, nowLeftMap);
-        RemoveMapList(centerMaps, nowCenterMap);
-        RemoveMapList(rightMaps, nowRightMap);
+        /* 리스트에서 맵 삭제 */
+        RemoveMapList(firstMaps, nowFirstMap);
+        RemoveMapList(secendMaps, nowSecendMap);
+        RemoveMapList(thirdMaps, nowThirdMap);
     }
 
-    private int MapSelect(List<Transform> map)
+    private int SelectMap(List<Transform> map)
     {
         int randomNum = Random.Range(0, map.Count);
         map[randomNum].gameObject.SetActive(true);
 
         return randomNum;
+    }
+
+    private Transform getPivot(Transform parent, string tagName)
+    {
+        Transform[] children = parent.GetComponentsInChildren<Transform>();
+
+        for (int i = 0; i < children.Length; i++)
+        {
+            if (children[i].CompareTag(tagName))
+                return children[i];
+        }
+        return null;
     }
 
     private void RemoveMapList(List<Transform> map, int index)
