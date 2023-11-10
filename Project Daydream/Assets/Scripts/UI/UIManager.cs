af3a1ff90394df53b23f22798c934dcb70d1cf87
CHJ000110 <chj000110@kakao.com>
2023-11-10 20:17:45 +0900

    [Feat] 스킬 UI, 아이템 UI 추가
    스킬, 아이템 UI 기능과 테스트용 변수들 추가

--- Project Daydream/Assets/Scripts/UI/UIManager.cs
+++ Project Daydream/Assets/Scripts/UI/UIManager.cs
@@ -9,9 +9,53 @@ public class UIManager : MonoBehaviour
     /* ------------- 컴포넌트 변수 ------------- */
     private PlayerMain playerMain;
     private PlayerController playerController;
-    private int maxHp;
+
+
+    //-------------아이템 관련 변수--------------------------------
+    [Header("아이템 관련 테스트 변수")]
+    [SerializeField, Range(0f, 3f)]
+    private int itemCount; // 현재 들고 있는 아이템 수
+    [SerializeField]
+    private bool availableItem1; // 아이템1의 사용가능 유무
+    [SerializeField]
+    private bool availableItem2; // 아이템2의 사용가능 유무
+    [SerializeField]
+    private bool availableItem3; // 아이템3의 사용가능 유무
+
+    private GameObject Item1;
+    private GameObject Item2;
+    private GameObject Item3;
+    //-------------스킬 관련 변수--------------------------------
+    private GameObject ASkill;
+    private GameObject SSkill;
+    private GameObject DSkill;
+    private float aSkillTimer;
+    private float sSkillTimer;
+    private float dSkillTimer;
+    private float ultimateSkillTimer;
+    [Space(10f)]
+    [Header("스킬 관련 테스트 변수")]
+    [SerializeField]
+    private bool isASkill; // 임시 A스킬 사용 유무 변수 
+    [SerializeField]
+    private bool isSSkill; // 임시 S스킬 사용 유무 변수 
+    [SerializeField]
+    private bool isDSkill; // 임시 D스킬 사용 유무 변수 
+    [SerializeField]
+    private bool isUltimateSkill; // 임시 궁스킬 사용 유무 변수 
+
+    //임시 스킬 쿨타임 변수
+    [SerializeField, Range(0f, 100f)]
+    private float aSkillColltime = 3f;
+    [SerializeField, Range(0f, 100f)]
+    private float sSkillColltime = 3f;
+    [SerializeField, Range(0f, 100f)]
+    private float dSkillColltime = 3f;
+    [SerializeField, Range(0f, 100f)]
+    private float ultimateSkillColltime = 5f;
 
     /* ---------------- 인스펙터 --------------- */
+    [Space(10f)]
     [Header("오브젝트 연결")]
     [SerializeField]
     private GameObject player;
@@ -23,6 +67,12 @@ public class UIManager : MonoBehaviour
     private GameObject MoonRockUI;
     [SerializeField]
     private GameObject DashStackUI;
+    [SerializeField]
+    private GameObject UltimitSkill;
+    [SerializeField]
+    private GameObject Skill;
+    [SerializeField]
+    private GameObject Item;
 
     /* -------------- 싱글톤 변수 -------------- */
     public static UIManager instance = null;
@@ -43,11 +93,71 @@ public class UIManager : MonoBehaviour
 
         playerMain = player.GetComponent<PlayerMain>();
         playerController = player.GetComponent<PlayerController>();
+        Item1 = Item.transform.GetChild(0).gameObject;
+        Item2 = Item.transform.GetChild(1).gameObject;
+        Item3 = Item.transform.GetChild(2).gameObject;
+
+        ASkill = Skill.transform.GetChild(0).gameObject;
+        SSkill = Skill.transform.GetChild(1).gameObject;
+        DSkill = Skill.transform.GetChild(2).gameObject;
 
         HP_Gage_Control();
         DashStackUI_Control();
         CoinUI_Control();
         MoonRockUI_Control();
+    }
+
+    private void FixedUpdate()
+    {
+        ShowItemUI();
+        if (isASkill)
+        {
+            aSkillTimer += Time.deltaTime;
+            ASkillUI();
+            if (aSkillTimer >= aSkillColltime)
+            {
+                isASkill = false;
+                aSkillTimer = 0;
+                ASkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.white;
+                return;
+            }
+        }
+        if (isSSkill)
+        {
+            sSkillTimer += Time.deltaTime;
+            SSkillUI();
+            if (sSkillTimer >= sSkillColltime)
+            {
+                isSSkill = false;
+                sSkillTimer = 0;
+                SSkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.white;
+                return;
+            }
+        }
+        if (isDSkill)
+        {
+            dSkillTimer += Time.deltaTime;
+            DSkillUI();
+            if (dSkillTimer >= dSkillColltime)
+            {
+                isDSkill = false;
+                dSkillTimer = 0;
+                DSkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.white;
+                return;
+            }
+        }
+        if (isUltimateSkill)
+        {
+            ultimateSkillTimer += Time.deltaTime;
+            UltimateSkillUI();
+            if (ultimateSkillTimer >= ultimateSkillColltime)
+            {
+                isUltimateSkill = false;
+                ultimateSkillTimer = 0;
+                UltimitSkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.white;
+                return;
+            }
+        }
     }
 
     public void HP_Gage_Control()
@@ -78,5 +188,65 @@ public class UIManager : MonoBehaviour
         {
             DashStackUI.transform.GetChild(j).gameObject.SetActive(true);
         }
+    }
+
+    public void ShowItemUI()
+    {
+        switch (itemCount)
+        {
+            case 0:
+                Item1.SetActive(false);
+                Item2.SetActive(false);
+                Item3.SetActive(false);
+                break;
+            case 1:
+                Item1.SetActive(true);
+                Item1.transform.GetChild(0).gameObject.SetActive(!availableItem1);
+                Item1.transform.GetChild(1).gameObject.SetActive(availableItem1);
+                Item2.SetActive(false);
+                Item3.SetActive(false);
+                break;
+            case 2:
+                Item1.SetActive(true);
+                Item1.transform.GetChild(0).gameObject.SetActive(!availableItem1);
+                Item1.transform.GetChild(1).gameObject.SetActive(availableItem1);
+                Item2.SetActive(true);
+                Item2.transform.GetChild(0).gameObject.SetActive(!availableItem2);
+                Item2.transform.GetChild(1).gameObject.SetActive(availableItem2);
+                Item3.SetActive(false);
+                break;
+            case 3:
+                Item1.SetActive(true);
+                Item1.transform.GetChild(0).gameObject.SetActive(!availableItem1);
+                Item1.transform.GetChild(1).gameObject.SetActive(availableItem1);
+                Item2.SetActive(true);
+                Item2.transform.GetChild(0).gameObject.SetActive(!availableItem2);
+                Item2.transform.GetChild(1).gameObject.SetActive(availableItem2);
+                Item3.SetActive(true);
+                Item3.transform.GetChild(0).gameObject.SetActive(!availableItem3);
+                Item3.transform.GetChild(1).gameObject.SetActive(availableItem3);
+                break;
+        }
+    }
+
+    public void ASkillUI()
+    {
+        ASkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.gray;
+        ASkill.transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = aSkillTimer / aSkillColltime;
+    }
+    public void SSkillUI()
+    {
+        SSkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.gray;
+        SSkill.transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = sSkillTimer / sSkillColltime;
+    }
+    public void DSkillUI()
+    {
+        DSkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.gray;
+        DSkill.transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = dSkillTimer / dSkillColltime;
+    }
+    public void UltimateSkillUI()
+    {
+        UltimitSkill.transform.GetChild(0).gameObject.GetComponent<Image>().color = Color.gray;
+        UltimitSkill.transform.GetChild(0).gameObject.GetComponent<Image>().fillAmount = ultimateSkillTimer / ultimateSkillColltime;
     }
 }
