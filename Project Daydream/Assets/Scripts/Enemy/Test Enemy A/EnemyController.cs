c96e3a0b66fe2ed4dd732d2b15c7c23bff04ea88
SKYBonus <dlgksmf0181@daum.net>
2023-11-11 23:18:17 +0900

    [Docs] 코드정리
    Chase -> PlayerChase 코루틴으로 변경
    Animation 가는방향 != 애니메이션 수정
    
    외 코드 정리

--- Project Daydream/Assets/Scripts/Enemy/Test Enemy A/EnemyController.cs
+++ Project Daydream/Assets/Scripts/Enemy/Test Enemy A/EnemyController.cs
@@ -4,101 +4,125 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private Rigidbody2D rigid;
-    private Animator anim;
-    private SpriteRenderer spriteRenderer;
-    private EnemyMain enemyMain;
+    protected Rigidbody2D rigid;
+    protected Animator anim;
+    protected SpriteRenderer spriteRenderer;
+    protected EnemyMain enemyMain;
 
-    /* -------------=--- AI관련 =--------------- */
-    private int nextMove; // 행동 지표를 결정할 변수 하나 생성
-    private int saveDir = 1;
+    /* ---------------- AI 관련 ---------------*/
+    private int nextMove; //행동지표
+    private Vector2 frontVec; //바닥 감지
+    private RaycastHit2D rayHit;
 
-    /* ---------------- 인스펙터 --------------- */
+    private bool chase = false; //플레이어 추적
+
+    /* ---------------- 인스펙터 ---------------*/
     [Header("설정")]
     [SerializeField, Range(0, 10)]
     private float maxSpeed = 2.5f;
     [SerializeField, Range(0, 10)]
     private float raycastDistance = 1f;
 
-    void Awake()
+    [SerializeField]
+    private Transform target; // 플레이어
+
+    /* ---------------- 코루틴 ---------------*/
+    IEnumerator enumerator; //선언하지 않으면 Stop,Start에 최종적으로 서로 다른 코루틴이 들어감
+
+    private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         spriteRenderer = GetComponentInChildren<SpriteRenderer>();
         enemyMain = GetComponent<EnemyMain>();
 
-        Invoke("Think", 3); // 주어진 시간이 지난뒤 지정된 함수를 실행하는 함수 
+
+        enumerator = PlayerChase();
+        Move();
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (!enemyMain.IsHit())
+        // 이동
+        rigid.velocity = new Vector2(nextMove * maxSpeed, rigid.velocity.y);
+
+        // 바닥 감지
+        frontVec = new Vector2(rigid.position.x + nextMove * raycastDistance, rigid.position.y);
+        rayHit = Physics2D.Raycast(frontVec, Vector3.down, raycastDistance, LayerMask.GetMask("Ground"));
+
+        if (rayHit.collider == null)// 바닥이 없을 때
         {
-            rigid.velocity = new Vector2(nextMove * maxSpeed, rigid.velocity.y);
-
-            //지형 체크
-            Vector2 frontVec = new Vector2(rigid.position.x + nextMove * raycastDistance, rigid.position.y);
-
-            //Debug.DrawRay(frontVec, Vector3.down, new Color(0, raycastDistance, 0)); // 에디터 상에서만 레이를 그려준다
-            RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, raycastDistance, LayerMask.GetMask("Ground"));
-            if (rayHit.collider == null) // 바닥 감지가 없을 때
-                Turn();
+            Turn();
         }
     }
 
-    void Think()
+    void Move() //방향 전환
     {
-        if (!enemyMain.IsHit())
+        if (!enemyMain.IsHit() && chase == false)
         {
+            nextMove = Random.Range(-1, 2);//-1, 0, 1
+            Animate();
+
+            float nextMoveTime = Random.Range(2f, 5f);
+            Invoke("Move", nextMoveTime);
+        }
+    }
+
+    /* ---------------- 플레이어 추적 ---------------*/
+    private void OnTriggerEnter2D(Collider2D collision) //들어옴
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            chase = true;
+            StartCoroutine(PlayerChase());
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision) //나감
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            chase = false;
+            StopCoroutine(PlayerChase());
             Move();
-
-            float nextThinkTime = Random.Range(2f, 5f); // 2.0 ~ 5.0
-            Invoke("Think", nextThinkTime);
         }
     }
 
-    void Move() // 이동 방향 결정
-    {
-        nextMove = Random.Range(-1, 2); // -1, 0, 1
-
-        if (nextMove == 1)  // 방향 저장용
-            saveDir = 1;
-        else if (nextMove == -1)
-            saveDir = -1;
-
-        Animate();
-    }
-
-    public void Turn() // 속도는 유지한채로 방향만 전환 -> 다음 액션까지 2초 연장
+    /* ---------------- 회전, 애니메이션(전환) ---------------*/
+    public void Turn()
     {
         nextMove *= -1;
         Animate();
-
-        CancelInvoke();
-        Invoke("Think", 2);
     }
 
-    void Animate() // 스프라이트 애니메이션 관련
+    void Animate()
     {
         anim.SetInteger("runSpeed", nextMove);
 
         if (nextMove != 0)
+        {
             transform.localScale = new Vector3(Mathf.Abs(transform.localScale.x) * -nextMove, transform.localScale.y, transform.localScale.z);
-        //spriteRenderer.flipX = nextMove == 1;
+        }
     }
 
-    public void lookBack()
-    {
-        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
-    }
 
-    public int getThink()
+    /* ---------------- 플레이어 추적 ---------------*/
+    IEnumerator PlayerChase()
     {
-        return nextMove;
-    }
+        while (chase == true)
+        {
+            yield return null;
 
-    public int getDir()
-    {
-        return saveDir;
+            //플레이어 방향
+            float dir2 = target.position.x - transform.position.x;
+            nextMove = (dir2 < 0) ? -1 : 1;
+
+            if (rayHit.collider == null)// 바닥이 없을 때 ( 없으면 그냥 떨어지는 현상 방지 )
+            {
+                Turn();
+            }
+            Animate();
+        }
     }
 }
+
