36cc829d51826f4e86b45030435d28ec313a7adb
Arkru <35932023+Arkru1204@users.noreply.github.com>
2023-11-23 03:33:40 +0900

    [Feat] 텍스트 중복 방지 / 출력 속도 증가
    * 출력 완료 후 텍스트 제거
    * 반복 추가

--- Project Daydream/Assets/Scripts/Map/PrintSubtitle.cs
+++ Project Daydream/Assets/Scripts/Map/PrintSubtitle.cs
@@ -5,17 +5,21 @@ using UnityEngine.UI;
 
 public class PrintSubtitle : MonoBehaviour
 {
-    /* ----------- 트리거 판정 변수 ------------ */
-    private bool hadTrigger = false; // 트리거 발동 유무
-
     /* ----------- 텍스트 관련 변수 ------------ */
-    private string fileContent;
     private Text textUI;
+    private string fileContent;
+
+    /* ----------- 트리거 판정 변수 ------------ */
+    private bool isTrigger = false;
 
     /* ---------------- 인스펙터 --------------- */
     [Header("오브젝트 연결")]
     [SerializeField]
     private TextAsset textFile;
+
+    [Header("반복 설정")]
+    [SerializeField]
+    private bool isRepeat = false;
 
     /* -------------- 이벤트 함수 -------------- */
     private void Start()
@@ -27,9 +31,9 @@ public class PrintSubtitle : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && !hadTrigger)
+        if (collision.CompareTag("Player") && !isTrigger && textUI.text == "")
         {
-            hadTrigger = true;
+            isTrigger = true;
             StartCoroutine(TextPrint(textUI, fileContent));
         }
     }
@@ -58,7 +62,8 @@ public class PrintSubtitle : MonoBehaviour
             writeText += textValue[i]; // 문자열 하나씩 끊어서 적기
             textUI.text = currentText + writeText; // 텍스트 UI에 텍스트 표시
 
-            yield return new WaitForSeconds(0.01f); // 글자마다 딜레이
+            //yield return null;
+            yield return new WaitForSeconds(0.15f); // 글자마다 딜레이
 
             typingCount++;
 
@@ -68,9 +73,14 @@ public class PrintSubtitle : MonoBehaviour
             {
                 currentText += "\n"; // 한 줄 완성 후 줄 바꿈
                 typingCount = 0;
+                yield return new WaitForSeconds(0.15f); // 줄 바꾼 후 딜레이
             }
-
-            yield return new WaitForSeconds(0.1f); // 줄 바꾼 후 딜레이
         }
+
+        yield return new WaitForSeconds(0.5f);
+        textUI.text = "";
+
+        if (isRepeat)
+            isTrigger = false;
     }
 }
